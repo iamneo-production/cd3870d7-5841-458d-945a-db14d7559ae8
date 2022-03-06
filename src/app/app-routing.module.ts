9a196d8f49280cfde1b107f01e0c76719a2d5a32
PriyankaDas98 <priyanka.55555das@gmail.com>
2022-03-06 16:07:38 +0000

    admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DisplayUserComponent } from './Admin/components/display-user/display-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'displayuser', component: DisplayUserComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
