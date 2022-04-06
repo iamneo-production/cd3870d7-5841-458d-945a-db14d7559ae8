6cbe81749309e5292135dbc8d8f922306547ffc5
Guhapriya-K <guhapriyagp06@gmail.com>
2022-04-06 06:11:50 +0000

    intial commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,88 +1,44 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { LoginComponent } from './pages/auth/login/login.component';
-// import { SignupComponent } from './pages/auth/signup/signup.component';
-// import { HomepageComponent } from './pages/customer/homepage/homepage.component';
-// import { AddCenterComponent } from './pages/admin/add-center/add-center.component';
-// import { NavbarComponent } from './components/navbar/navbar.component';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     LoginComponent,
-//     SignupComponent,
-//     HomepageComponent,
-//     AddCenterComponent,
-//     NavbarComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     BrowserAnimationsModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppointmentComponent } from './pages/customer/appointment/appointment.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { UpdateAppointmentComponent } from './pages/customer/update-appointment/update-appointment.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
-import { SignupComponent } from './pages/auth/signup/signup.component';
-
-
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import { LoginComponent } from './pages/auth/login/login.component';
-import { authInterceptorProviders } from './service/auth.interceptor';
-import { HomepageComponent } from './pages/customer/homepage/homepage.component';
-import { AddCenterComponent } from './pages/admin/add-center/add-center.component';
+import {MatSelectModule} from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    SignupComponent,
-    LoginComponent,
-    HomepageComponent,
-    AddCenterComponent
-    
+    AppointmentComponent,
+    UpdateAppointmentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    NgbModule,
+    HttpClientModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatSnackBarModule,
     MatButtonModule,
     MatInputModule,
-    MatFormFieldModule,
-    FormsModule,
-    HttpClientModule,
-    MatSnackBarModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
-    MatSlideToggleModule,
-    
+    ReactiveFormsModule
   ],
-  providers: [authInterceptorProviders],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
